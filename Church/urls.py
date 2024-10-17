69d6a69395e48aee0ba335a3fff3859a6ebcc66b
CodewithAndare-hub <139978681+CodewithAndare-hub@users.noreply.github.com>
2024-10-17 08:09:17 +0300

    views.py

--- Church/urls.py
+++ Church/urls.py
@@ -18,8 +18,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from member.views import *
+from member.views import Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Home.as_view(), name='home'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
